34973646b8d5c5d531ce4ed2022582c94190c303
Chris Banta <chrisbanta1@gmail.com>
2020-03-21 16:43:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,15 @@ new.push(yield(array[i]))
 end
    return new
 end
+
+
+
+def reduce(source_array, starting_point = 0)
+  i = 0
+  total = 0
+  while i < source_array.length do
+  total = yield total + source_array[i]
+  i += 1
+  end
+  return total + starting_point
+end
