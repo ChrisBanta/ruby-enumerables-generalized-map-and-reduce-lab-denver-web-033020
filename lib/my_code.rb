0fc012d030001a1f1e589aa1a55cd488a570342e
Chris Banta <chrisbanta1@gmail.com>
2020-03-21 16:51:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,8 +11,12 @@ end
 
 
 def reduce(source_array, starting_point = 0)
+ if starting_point
+  total = starting_point
   i = 0
-  total = 0
+  else
+  total = source_array[0]
+  i = 1
   while i < source_array.length do
   total = yield(total + source_array[i])
   i += 1
